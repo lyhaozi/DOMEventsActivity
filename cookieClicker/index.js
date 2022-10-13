0724654e7bbf5e276050dce2b5b56941ff4a0222
lyhaozi <lyhao1010@gmail.com>
2022-10-12 23:20:05 -0400

    Optimized our cookieclicker, let it looks a little bit better.

--- cookieClicker/index.js
+++ cookieClicker/index.js
@@ -1,16 +1,7 @@
-// const counter = document.querySelector('#counter')
-// const counterBox = document.createElement('counterBox')
-// counterBox.style.border = "1px solid blueviolet";
-const img = document.querySelector('.img')
-const button = document.querySelector('#img');
-// let number = document.querySelector('#number');
-let number 
-number = 0;
-img.addEventListener('click', function(){
-    console.log('clickme');
-number++;
-// button.innerHTML = counter
-document.querySelector('#number').innerHTML = number;
-}
-
-)
+let img = document.querySelector(".img");
+let number = 0;
+img.addEventListener("click", function () {
+  console.log("Clickme!");
+  number++;
+  document.querySelector("#number").innerHTML = number;
+});
